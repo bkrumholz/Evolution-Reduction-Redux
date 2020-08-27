4e6c3835699a2e7a1ff774a17335793b0dd9a6be
Brian Krumholz <brian.krumholz@gmail.com>
2020-08-27 10:07:19 -0700

    Initial commit

---
+++ Evolution Reduction_0.18.0/settings.lua
@@ -0,0 +1,31 @@
+data:extend{
+    {
+        type = "int-setting",
+        name = "Evolution-Reduction-Factor",
+        setting_type = "runtime-global",
+        default_value = 100,
+        maximum_value = 2000,
+        minimum_value = 0,
+        order = "1"
+    },
+	
+	{
+        type = "bool-setting",
+        name = "Evolution-Reduction-Worm",
+        setting_type = "runtime-global",
+        default_value = true,
+    },
+	
+	{
+        type = "bool-setting",
+        name = "Evolution-Reduction-Spawner",
+        setting_type = "runtime-global",
+        default_value = true
+    },
+	{
+        type = "bool-setting",
+        name = "Evolution-Reduction-Alien-Gear-For-War",
+        setting_type = "runtime-global",
+        default_value = true
+    },
+}
